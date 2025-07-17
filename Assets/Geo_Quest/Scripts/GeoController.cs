fa2220a99b1ab4ae587a90ff5fdda37baa753966
erba8 <manyb2308@gmail.com>
2025-07-17 00:20:10 -0400

    geo

--- Assets/Geo_Quest/Scripts/GeoController.cs
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -3,11 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-// when something is gray it mines its imported and not yet used
 
-// this class inherits from MonoBehaviour class
-// inheritance = extension vs. imports = Libraries
-// libraries/imports allow for things like inputs, physics, and sprite renderers
 
 public class GeoController : MonoBehaviour
 {
@@ -15,70 +11,22 @@ public class GeoController : MonoBehaviour
     //Variables
     //============================================
 
-    // global
-    int variableOne;
-    int variableTwo = 5;
-    string str1 = "Hello ";
-    public int varOne = 9; // public means it can be acessed and modified by other script
-    int challenge = 3;
 
     private Rigidbody2D rb;
     public int xSpeed = 5;
-    public string nextLevel = "Scene_2";
+    public string nextLevel = "Level_2";
+    public string lastLevel = "Level_3";
+    public string end = "End";
+    public SpriteRenderer sr;
     void Start()// Start is called before the first frame update
     {
-        int variableThree = 3; // local
-        string str2 = "World";
-        // used for initalization
-        Debug.Log("Hello World");
-        // Debug is a class from unity, use for debugging and displaying
-        // . is an access operater, allowing us to use functions or varibles from the debug class
-        // Log() is a method from the Debug class, sends message inside the Console
-
-        /*
-         * int Integer - 1;
-         * float Float = 3.14;
-         * bool Boolean = true;
-         * char Character = 'a';
-         * string String = "hello World";
-         
-
-        variableOne = 5;
-        print(variableTwo +  variableTwo);
-        Debug.Log(str1 + str2);
-        */
-
         rb = GetComponent<Rigidbody2D>();
-
-
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // runs every frame, like a function, this is where the logic goes
-        // Debug.Log(challenge);
-        //  challenge += 1;
-
-
-        //transform.position += new Vector3(0.005f, 0, 0);
-
-        //if(Input.GetKeyDown(KeyCode.W))
-        //{
-        //    transform.position += new Vector3(0, 1, 0);
-        //}
-        //if (Input.GetKeyDown(KeyCode.S))
-        //{
-        //    transform.position += new Vector3(0, -1, 0);
-        //}
-        //if (Input.GetKeyDown(KeyCode.A))
-        //{
-        //    transform.position += new Vector3(-1, 0, 0);
-        //}
-        //if (Input.GetKeyDown(KeyCode.D))
-        //{
-        //    transform.position += new Vector3(1, 0, 0);
-        //}
 
         if (Input.GetKeyDown(KeyCode.A))
         {
@@ -92,8 +40,25 @@ public class GeoController : MonoBehaviour
         float xInput = Input.GetAxis("Horizontal");
         //Debug.Log(xInput);
         rb.velocity = new Vector2 (xInput* xSpeed, rb.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            ChangeColor(Color.cyan);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            ChangeColor(Color.yellow);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            ChangeColor(Color.blue);
+        }
     }
 
+    private void ChangeColor(Color newColor)
+    {
+        sr.color = newColor; 
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // Debug.Log("hit");
@@ -116,9 +81,23 @@ public class GeoController : MonoBehaviour
                     SceneManager.LoadScene(thisLevel);
                     break;
                 }
+            case "Water":
+                {
+                    nextLevel = "Level_2";
+                    SceneManager.LoadScene(nextLevel);
+                    break;
+                }
+
             case "Finish":
                 {
-                    SceneManager.LoadScene(nextLevel);
+                    lastLevel = "Level_3";
+                    SceneManager.LoadScene(lastLevel);
+                    break;
+                }
+            case "Goal":
+                {
+                    end = "End";
+                    SceneManager.LoadScene(end);
                     break;
                 }
         }
