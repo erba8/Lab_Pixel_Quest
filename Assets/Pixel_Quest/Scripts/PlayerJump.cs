a0610b54c70d9b3837101b493dc946319da98674
erba8 <manyb2308@gmail.com>
2025-07-21 12:20:20 -0400

    day8

--- Assets/Pixel_Quest/Scripts/PlayerJump.cs
+++ Assets/Pixel_Quest/Scripts/PlayerJump.cs
@@ -5,14 +5,58 @@ using UnityEngine;
 public class PlayerJump : MonoBehaviour
 {
     // Start is called before the first frame update
+    private Rigidbody2D rb;
+    public float jumpForce = 10f;
+    public float fallForce = 4;
+    private Vector2 gravityVector;
+
+    public float CapsuleHeight = 0.25f;
+    public float CapsuleRadius = 0.08f;
+
+    public Transform feetCollider;
+    public LayerMask groundMask;
+    private bool _groundCheck;
+
+    private string waterTag = "Water";
+    private bool waterCheck;
     void Start()
     {
-        
+        gravityVector = new Vector2(0, Physics2D.gravity.y);
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        _groundCheck = Physics2D.OverlapCapsule(feetCollider.position,
+            new Vector2(CapsuleHeight, CapsuleRadius), CapsuleDirection2D.Horizontal,
+            0, groundMask);
+
+        if(Input.GetKeyDown(KeyCode.Space) && (_groundCheck || waterCheck))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        if(rb.velocity.y < 0 && !waterCheck)
+        {
+            rb.velocity += gravityVector * (fallForce * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag(waterTag))
+        {
+            waterCheck = true; 
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag(waterTag))
+        {
+            waterCheck = false;
+        }
     }
 }
