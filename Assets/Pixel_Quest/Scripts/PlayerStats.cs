83a404abd7eb779d6c69888bb04cd8a2617899d6
erba8 <manyb2308@gmail.com>
2025-07-22 12:14:15 -0400

    day9

--- Assets/Pixel_Quest/Scripts/PlayerStats.cs
+++ Assets/Pixel_Quest/Scripts/PlayerStats.cs
@@ -6,6 +6,9 @@ using UnityEngine.SceneManagement;
 public class PlayerStats : MonoBehaviour
 {
     public string nextLevel = "GeoLevel_2";
+    private int counter = 0;
+    private int health = 3;
+    public Transform RespawnPoint;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,24 +28,42 @@ public class PlayerStats : MonoBehaviour
 
         switch (collision.tag)
         {
-            case "Win":
-                {
-                    break;
-                }
-            case "Lose":
-                {
-                    break;
-                }
             case "Death":
                 {
-                    Debug.Log("Player Has Died");
-                    string thisLevel = SceneManager.GetActiveScene().name;
-                    SceneManager.LoadScene(thisLevel);
-                    break;
+                    //Debug.Log("Player Has Died");
+                    //string thisLevel = SceneManager.GetActiveScene().name;
+                    //
+                    health--;
+                    if (health <= 0)
+                    {
+                        string thisLevel = SceneManager.GetActiveScene().name;
+                        SceneManager.LoadScene(thisLevel);
+                    }
+                    else
+                    {
+                        transform.position = RespawnPoint.position;
+                    }
+                        break;
                 }
             case "Finish":
                 {
                     SceneManager.LoadScene(nextLevel);
+                    break;
+                }
+            case "Coin":
+                {
+                    counter++;
+                    Destroy(collision.gameObject);
+                    break;
+                }
+            case "Health":
+                {
+                    
+                    if (health < 3)
+                    {
+                        health++;
+                        Destroy(collision.gameObject);
+                    }
                     break;
                 }
         }
